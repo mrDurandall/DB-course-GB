e17997a914c57a90d06ab5d73399a7e762a07184
mrdurandall <mrdurandall@gmail.com>
2021-03-08 20:47:47 +0300

    Добавлены ответы на первое задание

---
+++ Task1/DB_create.sql
@@ -0,0 +1,18 @@
+﻿DROP DATABASE IF EXISTS example;
+-- Удаляем БД example, если она уже существует.
+CREATE DATABASE example;
+-- Создаем БД example
+
+USE example;
+-- Указываем БД example как рабочую
+
+DROP TABLE IF EXISTS users;
+-- Удаляем таблицу users если она уже существует
+CREATE TABLE users (
+id INT UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+name VARCHAR (255)
+);
+/* Создаем таблицу users с двумя столбцами:
+id - числовой тип данных без отрицательных значений, не может принимать значение NULL,
+назначим его первичным ключом таблицы, и зададим ему автоматическое присваивание значений;
+name - переменная строка с максимальной длиной 255 символов*/
